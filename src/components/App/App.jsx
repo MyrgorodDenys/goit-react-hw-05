36d028635123702fe550d32f0635afd5ab6890e6
MyrgorodDenys <dmyrte@gmail.com>
2024-03-22 22:11:02 +0200

    Finish

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import "../../../node_modules/modern-normalize/modern-normalize.css";
+
+import Navigation from "../Navigation/Navigation";
+import RouteSection from "../RouteSection/RouteSection";
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <RouteSection />
+    </>
+  );
+};
+
+export default App;
